5f4fe650ed882515eb46939647eade7de895ecf1
roverkalisl <roverkalisl@gmail.com>
2026-01-01 14:31:30 +0530

    Final production settings for images and deploy

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # ✅ ADD THIS
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
