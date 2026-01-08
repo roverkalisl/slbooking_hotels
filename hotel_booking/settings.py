f59ab67e9fb60e9e813db80ddbe0abc317639a7f
roverkalisl <roverkalisl@gmail.com>
2026-01-08 22:26:20 +0530

    Final production settings for images and deploy

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -1,7 +1,6 @@
 """
 Django settings for hotel_booking project.
 """
-
 from pathlib import Path
 import os
 from decouple import config
@@ -105,9 +104,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Cloudinary config
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'your_cloud_name',
-    'API_KEY': 'your_api_key',
-    'API_SECRET': 'your_api_secret',
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
