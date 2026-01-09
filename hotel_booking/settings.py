2fe1c26aac3ac27927d9f98cacb0939de7e0aded
roverkalisl <roverkalisl@gmail.com>
2026-01-09 22:46:33 +0530

    Update home page with Booking.com style search form

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -59,6 +59,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.site_stats',
+                
             ],
         },
     },
