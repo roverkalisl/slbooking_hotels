9caad35f0f4d995cae5209bc108ff22f13d58dee
roverkalisl <roverkalisl@gmail.com>
2026-01-11 13:21:02 +0530

    Add management command to create SiteStats record

---
+++ core/management/commands/create_stats.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from core.models import SiteStats
+
+class Command(BaseCommand):
+    help = 'Create initial SiteStats record if not exists'
+
+    def handle(self, *args, **options):
+        SiteStats.objects.get_or_create(pk=1, defaults={'total_views': 0})
+        self.stdout.write(self.style.SUCCESS('SiteStats record created/verified!'))
